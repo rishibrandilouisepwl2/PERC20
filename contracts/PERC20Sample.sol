930d72c0871abd1bfc9c7153f320d609119415b1
rishibrandilouisepwl2 <rishibrandilouisepwl2@hotmail.com>
2024-09-22 13:23:46 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("425c935e6c9bce644c6c0a0174d4d667529f5d585d1c3f83eaad2fc708ac3f11", "425c935e6c9bce644c6c0a0174d4d667529f5d585d1c3f83eaad2fc708ac3f11") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
